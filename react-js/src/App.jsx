833398d122f6f6fe2fca9f8354d90b014bde67e3
Alec Springel <58418733+alecspringel@users.noreply.github.com>
2022-09-17 11:33:47 -0700

    added React (JavaScript) blueprint

---
+++ react-js/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h3>Hello World</h3>
+      <p>Running Node v16.13.0</p>
+      <p>Webpack v5.74.0</p>
+    </div>
+  );
+}
+
+export default App;
